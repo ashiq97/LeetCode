a768c82d7e00554cd5062c28afdbed0e7ba7616b
Ashiqur Rahman <33044608+ashiq97@users.noreply.github.com>
2022-12-20 18:06:42 +0600

    add two sum with different solution

--- 1_Two Sum/392_Is_Subsequence.cs
+++ 1_Two Sum/392_Is_Subsequence.cs
@@ -6,13 +6,13 @@ namespace _1_Two_Sum
 {
     class _392_Is_Subsequence
     {
-        static void Main(string[] args)
-        {
-            var s = "abc";
-            var t = "ahbgdc";
-            var result = IsSubsequence(s,t);
-            Console.WriteLine($"{result}");
-        }
+        //static void Main(string[] args)
+        //{
+        //    var s = "abc";
+        //    var t = "ahbgdc";
+        //    var result = IsSubsequence(s,t);
+        //    Console.WriteLine($"{result}");
+        //}
         public static bool IsSubsequence(string sequence, string testArray)
         {
             var seqIndx = 0;
