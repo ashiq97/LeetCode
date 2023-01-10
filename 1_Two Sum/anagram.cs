2528fd543d652d3f8fa72fe92a556f5786e7f64d
Ashiq Ibn Faruk <ashiq_aust@outlook.com>
2023-01-10 17:59:07 +0600

    doing some code

---
+++ 1_Two Sum/anagram.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_Two_Sum
+{
+    internal class anagram
+    {
+        static void Main(string[] args)
+        {
+            var s = "aacc";
+            var t = "acaac";
+            var result = checkIsValidAnagram(s, t);
+            Console.WriteLine(result);
+
+        }
+
+        private static bool checkIsValidAnagram(string s, string t)
+        {
+            var hashTable = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(hashTable.ContainsKey(s[i]))
+                {
+                    hashTable[s[i]] = hashTable[s[i]] + 1;
+                }
+                else
+                {
+                    hashTable[s[i]] = 1;
+                }
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (hashTable.ContainsKey(t[i]))
+                {
+                    hashTable[t[i]] = hashTable[t[i]] - 1;
+                    if (hashTable[t[i]] == 0)
+                    {
+                        hashTable.Remove(t[i]);
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+
+            return hashTable.Count == 0;
+        }
+    }
+}
---
+++ 1_Two Sum/palindrome.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_Two_Sum
+{
+    internal class palindrome
+    {
+        //static void Main(string[] args)
+        //{
+        //    var result = CheckPalindrome(1221);
+        //    Console.WriteLine(result);
+        //    var strResult = CheckStringPlindrome("abbcbba");
+        //    Console.WriteLine(strResult);
+
+        //}
+
+        private static bool CheckStringPlindrome(string str)
+        {
+            var strLength = str.Length;
+            var isPlaindrome = true;
+
+            for (int i = 0; i < strLength/2; i++)
+            {
+                if (str[i] != str[strLength - i-1])
+                {
+                    isPlaindrome= false;
+                    break;
+                }    
+            }
+
+            return isPlaindrome;
+
+        }
+
+        private static bool CheckPalindrome(int n)
+        {
+            var originalNumber = n;
+            var reversed = 0;
+            while(n> 0)
+            {
+                var lastDigit=n%10;
+                reversed = reversed * 10 + lastDigit;
+                n = n/ 10;
+            }
+
+            if(originalNumber == reversed)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
