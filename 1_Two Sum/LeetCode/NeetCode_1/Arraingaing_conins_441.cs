241b56ef81064f8faa0a1a027133dc89bc34ef26
Ashiq Ibn Faruk <ashiq_aust@outlook.com>
2023-05-01 18:02:28 +0600

    perfect square

---
+++ 1_Two Sum/LeetCode/NeetCode_1/Arraingaing_conins_441.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_Two_Sum.LeetCode.NeetCode_1
+{
+    internal class Arraingaing_conins_441
+    {
+        static void Main(string[] args)
+        {
+            var coins = 10;
+           var output =  ArrangeCoins(coins);
+            Console.WriteLine(output);
+        }
+        private static int ArrangeCoins(int n)
+        {
+            var c = 0;
+            var sum = 0;
+            var length = n;
+            
+            for (int i = 1; i <= length; i++)
+            {
+                var res = n - i;
+                n = res;
+
+                if (res == 0)
+                    return i;
+                else if (res < 0)
+                    return i - 1;
+            }
+
+            return c;
+        }
+    }
+   
+}
---
+++ 1_Two Sum/LeetCode/NeetCode_1/perfectSquare_367.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_Two_Sum.LeetCode.NeetCode_1
+{
+    public  class perfectSquare_367
+    {
+        //static void Main(string[] args)
+        //{
+        //    var input = 16;
+        //    var output = IsPerfectSquare(input);
+        //    Console.WriteLine(output);
+        //}
+
+        public static bool IsPerfectSquare(int n)
+        {
+            long left = 1;
+            long right = n;
+            
+            while(left <= right)
+            {
+                var mid = Convert.ToInt64((left + right) / 2);
+
+                if (mid * mid > n)    // 8*8 = 64 > 16
+                    right = mid - 1; // 8-1 = 7
+                else if (mid * mid < n) // 2*2 = 4 < 16
+                    left = left + 1;
+                else
+                    return true;
+            }
+            return false;
+        }
+    }
+}
